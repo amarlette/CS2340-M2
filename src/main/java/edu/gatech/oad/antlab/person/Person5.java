64f511f8c16faf4fe1ef865c288a2700b2a3d5eb
Chidambaram Kadiresan <ckadiresan@outlook.com>
2018-01-30 22:38:19 -0500

    fixed compile error in Person5, ckadiresan3.gradle -> build.gradle

--- src/main/java/edu/gatech/oad/antlab/person/Person5.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person5.java
@@ -3,22 +3,22 @@ package edu.gatech.oad.antlab.person;
 /**
  *  A simple class for person 5
  *  returns their name and a
- *  modified string 
- *  
+ *  modified string
+ *
  *  @author Bob
  *  @version 1.1
  */
 public class Person5 {
-  /** Holds the persons real name */
-  private String name;
+	/** Holds the persons real name */
+	private String name;
   	/**
 	 * The constructor, takes in the persons
 	 * name
 	 * @param pname the person's real name
 	 */
-  public Person5(String pname) {
-    name = pname;
-  }
+  	public Person5(String pname) {
+  		name = pname;
+  	}
   	/**
 	 * This method should take the string
 	 * input and return its characters rotated
@@ -29,38 +29,35 @@ public class Person5 {
 	 * @param input the string to be modified
 	 * @return the modified string
 	 */
-	private String calc(String input) {
-	    if (input.length() == 1 || input.length() == 2)
-		{
-		    return input;
-		}
+  	private String calc(String input) {
+  		if (input.length() == 1 || input.length() == 2) {
+  			return input;
+  		} else {
+  			String rotated = "";
+  			int index = 2;
+  			for (int i = 0; i < input.length(); i++) {
 
-	    } else {
-		String rotated = "";
-		int index = 2;
-		for (int i = 0; i < input.length(); i++) {
+  				if (index < input.length()) {
+  					rotated += input.charAt(index);
+  				} else {
+  					rotated += input.charAt(index - input.length());
+  				}
+  				index += 1;
+  			}
+  			return rotated;
+  		}
+  	}
 
-		    if (index < input.length()) {
-			rotated += input.charAt(index);
-		    } else {
-			rotated += input.charAt(index - input.length());
-		    }
-		    index += 1;
-		}
-		return rotated;
-	    }
-	}
-	
 	/**
 	 * Return a string rep of this object
 	 * that varies with an input string
 	 *
 	 * @param input the varying string
-	 * @return the string representing the 
+	 * @return the string representing the
 	 *         object
 	 */
 	public String toString(String input) {
-	  return name + calc(input);
+		return name + calc(input);
 	}
 
 }
